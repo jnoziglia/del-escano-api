986b861c0ecabfe51f082deb46cea072a9127781
Julian Noziglia <jnoziglia@codicilabs.com>
2023-08-09 18:12:57 -0300

    Adding route to edit party

--- api/route/home.py
+++ api/route/home.py
@@ -24,6 +24,21 @@ def add_party():
     return resp, 204
 
 
+@app_file1.route("/parties/<id>", methods=['PUT'])
+def edit_party(id):
+    schema = PartySchema()
+    party = Party.get_by_id(id)
+    party_json = request.get_json()
+    if party:
+        party.name = party_json['name'] if 'name' in party_json else party.name
+        party.votes = party_json['votes'] if 'votes' in party_json else party.votes
+    else:
+        party = schema.load(party_json)
+    party.save()
+    resp = schema.dump(party)
+    return resp, 204
+
+
 @app_file1.route("/seats", methods=['GET'])
 def get_seats():
     parties = Party.get_all()
