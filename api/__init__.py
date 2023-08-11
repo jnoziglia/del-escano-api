c19ccc413e146870955bb7860355cef2b82776a1
Julian Noziglia <jnoziglia@codicilabs.com>
2023-08-11 05:20:58 -0300

    Refactoring app entry point, changing folder structure

--- api/__init__.py
+++ api/__init__.py
@@ -5,17 +5,29 @@ from api.route.home import app_file1
 from api.db import db
 
 
-def create_app(config):
+def create_app(test_config=None):
+    # create and configure the app
     app = Flask(__name__)
+    app.config.from_object(os.getenv('APP_SETTINGS_MODULE'))
 
-    app.config.from_object(config)
-    db.init_app(app)
+    if test_config is None:
+        db.init_app(app)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
     ma.init_app(app)
     migrate.init_app(app, db)
 
     app.register_blueprint(app_file1)
 
     # register_error_handlers(app)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     return app
 
--- app.py
+++ app.py
@@ -1,4 +1,2 @@
-import os
 from api import create_app
-settings_module = os.getenv('APP_SETTINGS_MODULE')
-app = create_app(settings_module)
+app = create_app()
