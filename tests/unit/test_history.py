bb58d262ad62d8b7d6bf0f68bd48b5f7455d16f1
Julian Noziglia <jnoziglia@codicilabs.com>
2023-08-11 17:28:34 -0300

    Correcting history tests, delete history is no longer possible

--- tests/unit/test_history.py
+++ tests/unit/test_history.py
@@ -8,11 +8,3 @@ def test_get_history(client, populate_history):
     response = client.get('/history')
     assert response.status_code == 200
     assert len(response.json) == 1
-
-
-def test_delete_history(client, populate_history):
-    response = client.delete('/history/1')
-    assert response.status_code == 204
-    response = client.get('/history')
-    assert response.status_code == 200
-    assert response.json == []
